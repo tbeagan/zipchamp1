c8673b6716723e50f67a3c2534918afac9397486
tbeagan <tbeagan@adams.edu>
2019-03-02 20:01:32 -0700

    paths

--- zipchamp1/lambda.js
+++ zipchamp1/lambda.js
@@ -9,16 +9,15 @@ let fs = require("fs");
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-    /* The request payload will take the following format:
+
     {
-        "path": "path/to/zip/file/within/bucket",
+        "path": "tyler-test123",
         "changes": {
-            "path/to/new/file/1": "content for file 1",
-            "path/to/existing/file/2": "new content of file 2",
-            "path/to/deleted/file/3": null
+            "tyler-test-1234": "content for file 1",
+            "tyler-test-1234": "new content of file 2",
+            "tyler-test-1234": null
         }
     }
-     */
 
     let changes = event.changes;
     let modified = 0, removed = 0;
